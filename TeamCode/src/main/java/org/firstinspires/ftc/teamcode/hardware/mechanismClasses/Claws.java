9108aadcf26dd4b946db012356c470b5e8c42931
nateschmelkin <nate@schmelkin.com>
2023-04-10 14:28:02 -0400

    Fixed Claws to be both

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/mechanismClasses/Claws.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/mechanismClasses/Claws.java
@@ -8,24 +8,31 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.drive.GamepadConstants;
 
-public class FrontClaw extends Mechanism {
+public class Claws extends Mechanism {
 
     ContinuousServoIntake frontIntake;
+    ContinuousServoIntake backIntake;
 
     @Override
     public void init(HardwareMap hwMap) {
         frontIntake.initialize(hwMap, "frontLeftClaw", "frontRightClaw");
+        backIntake.initialize(hwMap, "backLeftClaw", "backRightClaw");
+
         frontIntake.setDirections(DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD);
+        backIntake.setDirections(DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
     public void loop(Gamepad gamepad) {
         if (gamepad.a || gamepad.right_trigger > GamepadConstants.GAMEPAD1_TRIGGER_DEADZONE) {
             frontIntake.intake(1);
+            backIntake.intake(1);
         } else if (gamepad.b || gamepad.left_trigger > GamepadConstants.GAMEPAD1_TRIGGER_DEADZONE) {
             frontIntake.outtake(1);
+            backIntake.outtake(1);
         } else {
             frontIntake.stop();
+            backIntake.stop();
         }
     }
 }
